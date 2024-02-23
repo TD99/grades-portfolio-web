d931cebc5658e2aca2d86ec590b32fb527aa23e7
Tim Dürr <tim@duerrs.ch>
2024-02-23 23:14:45 +0100

    feat: Initialized Vaadin/Hilla

---
+++ src/main/java/com/example/application/Application.java
@@ -0,0 +1,30 @@
+package com.example.application;
+
+import com.vaadin.flow.component.page.AppShellConfigurator;
+import com.vaadin.flow.server.AppShellSettings;
+import com.vaadin.flow.server.PWA;
+import com.vaadin.flow.theme.Theme;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * The entry point of the Spring Boot application.
+ * <p>
+ * Use the @PWA annotation make the application installable on phones, tablets
+ * and some desktop browsers.
+ */
+@SpringBootApplication
+@Theme(value = "hilla-react-app")
+@PWA(name = "Grade Portfolio", shortName = "Grade Portfolio")
+public class Application implements AppShellConfigurator {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Application.class, args);
+    }
+
+    @Override
+    public void configurePage(AppShellSettings settings) {
+        settings.addFavIcon("icon", "icons/icon.png", "192x192");
+        settings.addLink("shortcut icon", "icons/icon.png");
+    }
+}
