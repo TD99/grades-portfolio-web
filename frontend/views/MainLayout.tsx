b51093162213252a76f09a460f8880a6a9d221f5
Tim Dürr <tim@duerrs.ch>
2024-02-23 23:54:17 +0100

    go back

--- frontend/views/MainLayout.tsx
+++ frontend/views/MainLayout.tsx
@@ -1,10 +1,10 @@
-import {AppLayout} from '@hilla/react-components/AppLayout.js';
+import {AppLayout, AppLayoutDrawerOpenedChangedEvent} from '@hilla/react-components/AppLayout.js';
 import {DrawerToggle} from '@hilla/react-components/DrawerToggle.js';
 import {Item} from '@hilla/react-components/Item.js';
 import {Scroller} from '@hilla/react-components/Scroller.js';
 import Placeholder from 'Frontend/components/placeholder/Placeholder.js';
 import {MenuProps, routes, useViewMatches, ViewRouteObject} from 'Frontend/Router';
-import React, {Suspense} from 'react';
+import React, {Suspense, useState} from 'react';
 import {NavLink, Outlet} from 'react-router-dom';
 import {useTranslation} from "react-i18next";
 import css from './MainLayout.module.css';
@@ -19,6 +19,7 @@ type MenuRoute = ViewRouteObject &
   }>;
 
 export default function MenuOnLeftLayout() {
+  const [isDrawerOpened, setIsDrawerOpened] = useState(false);
   const matches = useViewMatches();
   const {t} = useTranslation();
   const currentTitle = matches[matches.length - 1]?.handle?.title ?? 'Unknown';
@@ -27,10 +28,16 @@ export default function MenuOnLeftLayout() {
     (route) => route.path && route.handle && route.handle.icon && route.handle.title
   ) as readonly MenuRoute[];
 
+  const handleDrawerOpenedChanged = (event: AppLayoutDrawerOpenedChangedEvent) => {
+    setIsDrawerOpened(event.detail.value);
+  }
 
   return (
-    <AppLayout className="block h-full" primarySection="navbar">
-      <header slot="drawer">
+    <AppLayout className="block h-full" primarySection="drawer" onDrawerOpenedChanged={handleDrawerOpenedChanged}>
+      <header slot="drawer" style={{padding: "0"}}>
+        {
+          isDrawerOpened && <DrawerToggle className={css.circle} slot="navbar" aria-label="Menu toggle"/>
+        }
         <h1 className="text-l m-0">{t("name")}</h1>
       </header>
       <Scroller slot="drawer" scroll-direction="vertical">
@@ -63,7 +70,24 @@ export default function MenuOnLeftLayout() {
         <LanguageSelector style={{width: '100%'}}/>
       </footer>
 
-      <DrawerToggle className={css.circle} slot="navbar" aria-label="Menu toggle"/>
+      {
+        isDrawerOpened ? (
+          <div
+            style={{
+              width: 'var(--lumo-size-l)',
+              height: 'var(--lumo-size-l)',
+              margin: '0 var(--lumo-space-s)',
+              display: 'flex',
+              justifyContent: 'center',
+              alignItems: 'center'
+            }} slot="navbar">
+            <span className={css.navicon}
+                  style={{maskImage: `url('line-awesome/svg/${matches[matches.length - 1]?.handle?.icon}.svg')`}}/>
+          </div>
+        ) : (
+          <DrawerToggle className={css.circle} slot="navbar" aria-label="Menu toggle"/>
+        )
+      }
       <div slot="navbar" className={css.navbardiv}>
         <h2 className="text-l m-0">
           {translateIf_i18n(currentTitle, t)}
