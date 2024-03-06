22ec9ec061a3377554854684905a626a6e9667cc
Tim Dürr <tim@duerrs.ch>
2024-03-06 15:37:17 +0100

    chore: changed package names and structure

--- src/main/java/ch/timduerr/gradesportfolio/endpoints/helloreact/HelloReactEndpoint.java
+++ src/main/java/ch/timduerr/gradesportfolio/endpoints/helloreact/HelloReactEndpoint.java
@@ -1,4 +1,4 @@
-package com.example.application.endpoints.helloreact;
+package ch.timduerr.gradesportfolio.endpoints.helloreact;
 
 import com.vaadin.flow.server.auth.AnonymousAllowed;
 import dev.hilla.Endpoint;
